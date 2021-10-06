bda86a853cc29bad2e3f2558743ae210ffad6d58
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-06 20:59:03 +0530

    uploading all folders to github

--- src/com/bl/tictoctoe/game/Tic_Toc_Toe.java
+++ src/com/bl/tictoctoe/game/Tic_Toc_Toe.java
@@ -1,14 +1,17 @@
 package com.bl.tictoctoe.game;
+
 public class Tic_Toc_Toe {
 	static char[] board =new char[10];// declaring array board globally
-	public static void main(String[] args) {		board();//calling the method board in main method
+	public static void main(String[] args) {
+	board();//calling the method board in main method
 	}
 	// creating a board to play
 	public static void board() 
 	{
-		// giving each array index value as empty initiall		for(int i=1;i<=10;i++) {
-		board[i]=' ';
+		// giving each array index value as empty initially
+    for(int i=1;i<=10;i++) {
+    		board[i]=' ';
+    	}
 	}
-}
 
 }
